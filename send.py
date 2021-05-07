008f573a9397cbe055d0945518f11b38604be58d
Kirill Bondar <42541852+JeySee3@users.noreply.github.com>
2021-05-07 12:24:51 +0300

    Create send.py

---
+++ send.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import smtplib as smtp
+from getpass import getpass
+
+email = input('введите почту: \n')
+password = getpass('введите пароль: ')
+dest_email = input('введите адрес получателя: \n')
+subject = input('тема письма: \n')
+email_text = input('текст письма: \n' )
+
+message = 'From: {}\nTo: {}\nSubject: {}\n\n{}'.format(email,
+                                                       dest_email,
+                                                       subject,
+                                                       email_text)
+
+server = smtp.SMTP_SSL('smtp.yandex.com', 465)
+server.set_debuglevel(1)
+server.ehlo(email)
+server.starttls()
+server.login(email, password)
+server.auth_plain()
+server.sendmail(email, dest_email, message)
+server.quit()
